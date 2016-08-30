af817033a224d0ba4774a19a865ee5deebf7a9c0
Mohamed Ibrahim <msami.fawzi@gmail.com>
2016-08-31 02:19:59 +0300

    Adding CVE query test

--- dalton/test/testcve.go
+++ dalton/test/testcve.go
@@ -1,1 +1,24 @@
-package test
+package main
+
+import (
+	"labix.org/v2/mgo/bson"
+	"dalton/db"
+	"fmt"
+)
+
+func main() {
+
+	query := bson.M{"products":bson.M{"$regex":"httpd:[0-9]+"}}
+	results , err := db.SearchCVEs(query,0,-1)
+
+	if err != nil {
+		fmt.Println("Received the following error , " , err)
+		return
+	}
+	fmt.Println(len(results))
+
+	for _ , cve := range results {
+
+		fmt.Println(cve.Product)
+	}
+}
